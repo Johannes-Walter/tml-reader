08e2cdecbdc0b0506017024e4166932677304892
Johannes-Walter <jo.walter97@gmx.de>
2021-05-10 17:14:43 +0200

    initial structure of the tml-reader

--- tml-reader.py
+++ tml-reader.py
@@ -1,10 +1,10 @@
-def is_valid(file: str) -> bool:
-    pass
+def read_tml(file: str = "sample.tml"):
+    # open file with file reader
+    
+    # delegate evereything in the file to the find_elements-function
+    # to find all children in the code
 
-def renderer(file: str) -> None:
-    if not is_valid(file):
-        return None
 
-class node():
-    def __init__():
-        pass
+def find_elements(text: str):
+    # find each pair of brackets in the given text and call find_elements
+    # recursively with the content
