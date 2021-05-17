008c9133529f9e937aed85b90f5876fe6bc8bb34
Johannes-Walter <jo.walter97@gmx.de>
2021-05-17 18:18:25 +0200

    First working version! Don't try to change anything in sample.tml, it will break :P

--- tml-reader.py
+++ tml-reader.py
@@ -1,24 +1,61 @@
 import turtle
 
 class shape():
+    def __init__(self):
+        self.sub_shapes = list()
+        
+    def append_shape(self, shape):
+        self.sub_shapes.append(shape)
+
     def prepare(self):
+        print(self.__dict__)
         turtle.penup()
         turtle.setposition(self.x_pos, self.y_pos)
         turtle.pencolor(self.border_color)
         turtle.width(self.border_width)
         turtle.setheading(self.angle)
         turtle.pendown()
+
+    def draw(self):
+        for shape in self.sub_shapes:
+            shape.draw()
+
+
+class image(shape):
+    def __init__(self, 
+                 lower_left_x: int = 0, lower_left_y: int = 0, 
+                 upper_right_x: int = 250, upper_right_y: int = 250,
+                 background_color: str = None):
+        super().__init__()
+        self.lower_left_x = lower_left_x
+        self.lower_left_y = lower_left_y
+        self.upper_right_x = upper_right_x
+        self.upper_right_y = upper_right_y
+        self.background_color = background_color
+
+    def draw(self):
+        turtle.setworldcoordinates(self.lower_left_x,
+                                   self.lower_left_y,
+                                   self.upper_right_x,
+                                   self.upper_right_y)
         
-        
-    def draw():
-        pass
+        # TODO: Screensize stimmend machen. Viel Glück dabei.
+        turtle.screensize(self.upper_right_y - self.lower_left_y,
+                          self.upper_right_x - self.lower_left_x)
+        if self.background_color is not None:
+            turtle.bgcolor(self.background_color)
+        super().draw()
+        turtle.done()
+        turtle.bye()
     
+
 class circle(shape):
-    def __init__(self, x_pos: int, y_pos: int,
-                 radius: int,
+    def __init__(self, x_pos: int = None, y_pos: int = None,
+                 radius: int = None,
                  border_width: int = 1,
                  border_color: str = "black",
                  fill_color: str = None):
+        super().__init__()
         self.x_pos = x_pos
         self.y_pos = y_pos - radius
         self.radius = radius
@@ -38,13 +75,15 @@ class circle(shape):
             turtle.end_fill()
         else:
             turtle.circle(self.radius)
+        super().draw()
 
 
 class line(shape):
-    def __init__(self, x_pos: int, y_pos: int,
-                 angle: int, length: int,
+    def __init__(self, x_pos: int = None, y_pos: int = None,
+                 angle: int = None, length: int = None,
                  border_width: int = 1,
                  border_color: str = "black"):
+        super().__init__()
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.angle = angle
@@ -55,15 +94,17 @@ class line(shape):
     def draw(self):
         self.prepare()
         turtle.forward(self.length)
+        super().draw()
 
 
 class rectangle(shape):
     def __init__(self,
-                 x_pos: int, y_pos: int,
-                 angle: int, 
-                 height: int, width: int,
+                 x_pos: int = None, y_pos: int = None,
+                 angle: int = 0, 
+                 height: int = None, width: int = None,
                  border_color: str = "black", border_width: int = 1,
                  fill_color: str = None):
+        super().__init__()
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.angle = angle
@@ -72,6 +113,7 @@ class rectangle(shape):
         self.border_color = border_color
         self.border_width = border_width
         self.fill_color = fill_color
+        
     
     def draw(self):
         self.prepare()
@@ -87,11 +129,8 @@ class rectangle(shape):
             
         if self.fill_color is not None:
             turtle.end_fill()
+        super().draw()
 
-rect = rectangle(0, 0, -45, 100, 50, fill_color="green")
-rect.draw()
-turtle.done()
-turtle.bye()
 
 class Tag():
     def find_tag(text, start: int, end: int):
@@ -103,7 +142,7 @@ class Tag():
             tag.find_opening_tag(text, start, end)
         except ValueError:
             return None
-        print(tag.tag_name)
+        #print(tag.tag_name)
         tag.find_closing_tag(text, tag.opening_tag_end, end)
         if hasattr(tag, "closing_tag_start") and hasattr(tag, "closing_tag_end"):
             return tag
@@ -149,22 +188,43 @@ def read_tml(file_path: str):
     with open(file_path) as file:
         # delegate evereything in the file to the find_elements-function
         # to find all children in the code
-        find_elements(file.read())
+        text = file.read()
+        tag = Tag.find_tag(text, start=0, end=len(text))
+
+        shape = get_shape(tag.tag_name)
+        find_elements(text, shape, tag.opening_tag_end, tag.closing_tag_start)
+    shape.draw()
 
 
-def find_elements(text: str, start = 0, end = None):
-    if end is None:
-        end = len(text)
-    #print(text)
-    #print(text)
+def get_shape(shape_name: str):
+    if shape_name.lower() == "circle":
+        return circle()
+    if shape_name.lower() == "rectangle":
+        return rectangle()
+    if shape_name.lower() == "line":
+        return line()
+    if shape_name.lower() == "image":
+        return image()
+    #print(shape_name)
+    return None
+
+
+def find_elements(text: str, shape, start: int, end: int):
+    #print(start, end)
     tag = Tag.find_tag(text, start, end)
     while tag:
-        find_elements(text, tag.opening_tag_end, tag.closing_tag_start)
+        sub_shape = get_shape(tag.tag_name)
+        if sub_shape is not None:
+            shape.append_shape(sub_shape)
+        else:
+            print(tag.tag_name, text[tag.opening_tag_end+1:tag.closing_tag_start])
+            setattr(shape, tag.tag_name, int(text[tag.opening_tag_end+1:tag.closing_tag_start]))
         
+        find_elements(text, sub_shape, tag.opening_tag_end, tag.closing_tag_start)
         tag = Tag.find_tag(text, tag.closing_tag_end, end)
     # find each pair of brackets in the given text and call find_elements
     # recursively with the content
     # print("fertig")
 
 
-#read_tml("sample.tml")
+read_tml("sample.tml")
